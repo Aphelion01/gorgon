84a1b88b136400881c22daad63dcc7c38a5249fd
winocm <rms@velocitylimitless.org>
2013-05-03 10:11:13 -0500

    [Foundation] Fix typographical errors present in ..all of the files.

--- Foundation/Payload/limera1n.c
+++ Foundation/Payload/limera1n.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2013, winocm. <rms@velocitylimitless.org
+ * Copyright (c) 2013, winocm. <rms@velocitylimitless.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
